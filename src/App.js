910bb422c6833a82743e1ce8d188850a4fe0f1e7
Erik Scow <erikscow@gmail.com>
2020-02-04 20:32:31 -0800

    Added state for data and updated file structure based on data content.

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios"
 
+//import components
+import Header from "./Components/Header"
+import Card from "./Components/Card"
+
 function App() {
-  const fetchData = (api) => {
+
+  //setting up state for the data
+  const [data, setData] = useState()
+
+  const fetchData = () => {
     axios
-      .get(api)
-      .then(response => console.log(response.data))
+      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(response => {
+        console.log(response.data)
+        setData(response.data)
+      })
       .catch(error => console.log(error))
   }
   //useEffect to call the data upon first render
-  useEffect(()=>{
-    axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-      .then(response => console.log(response.data))
-      .catch(error => console.log(error))
-    }, [])
+  useEffect(fetchData, [])
   
+  console.log(data)
   
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Header />
+      <Card data={data}/>
     </div>
   );
 }
