19be918c49c53460e7287c3387eac8f4bf2ab867
Erik Scow <erikscow@gmail.com>
2020-02-04 20:02:04 -0800

    Created file structure and successfully retrieved data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios"
 
 function App() {
+  const fetchData = (api) => {
+    axios
+      .get(api)
+      .then(response => console.log(response.data))
+      .catch(error => console.log(error))
+  }
+  //useEffect to call the data upon first render
+  useEffect(()=>{
+    axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(response => console.log(response.data))
+      .catch(error => console.log(error))
+    }, [])
+  
+  
   return (
     <div className="App">
       <p>
---
+++ src/Components/Card-Container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardContainer = () => {
+    return(
+    <div className="CardContainer">
+
+    </div>
+    )
+}
+
+export default CardContainer
