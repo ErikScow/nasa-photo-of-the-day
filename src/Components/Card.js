50f3c36abadb852c73146dcfbf78d97664cefd81
Erik Scow <erikscow@gmail.com>
2020-02-05 18:08:04 -0800

    Added Styling to the react page

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,11 +4,17 @@ const Card = (props)  => {
     const data = props.data
     console.log(data)
     return(
-    <div className="Card">
-        <h2>{data.title}</h2>
-        <p>{data.date}</p>
-        <img src={data.url} alt="Image of the day"/>
-        <p>{data.explanation}</p>
+        <div className="card-container">
+        <div className="Card">
+            <h2 className="card-title">{data.title}</h2>
+            <p className="card-date">{data.date}</p>
+            <img className="card-image" src={data.url} alt="Image of the day"/>
+            <div className="text-container">
+                <h3>Explanation:</h3>
+                <p className="card-text">{data.explanation}</p>
+            </div>
+            
+        </div>
     </div>
     )
 }
